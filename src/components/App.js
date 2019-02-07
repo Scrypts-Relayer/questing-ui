f58fa71421f34546ceefa7042cc3a130664cd68f
ian-jh <ianlapham@gmail.com>
2019-02-06 20:31:04 -0500

    update with policy data

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import '../App.scss'
 import Web3 from 'web3';
-import { Container, Row, Col,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink } from 'reactstrap';
-
-import Footer from './Footer';
-import Page from './Page';
-
 import { address, abi } from '../assets/contract';
 import { setupWeb3 } from '../services/services';
+import Nav from './Nav'
+import Create from './Create';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
-    let pageDict;
-    if (window.location.pathname === '/about' || window.location.pathname === '/about/') {
-      pageDict = 3
-    } else if (window.location.pathname === '/create' || window.location.pathname === '/create/') {
-      pageDict = 2
-    } else {
-      pageDict = 1
-    }
     this.state = {
       isConnected : true,
       web3 : null,
       contract : null,
       account : null,
       isOpen: false,
-      page: pageDict // 1:Quest, 2:Create, 3:About
     };
   }
 
@@ -55,46 +38,24 @@ class App extends Component {
       isOpen: !this.state.isOpen
     });
   }
-  /*
-  *
-  * THIS FUNCTION WILL BE MOVED TO SERVICES
-  * THIS FUNCTION IS HERE RN JUST TO HELP YOU DEVELOP, @IAN
-  *
-  */
+
   getQuests = () => {
     return ["quest to alabama", "nola quest", "i love me some quests quest"]
   }
+
   render() {
     return (
-      <div id="App">
-        <Navbar id="nav" color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink onClick={() => {this.setState({page: 1})}}>Quest</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink onClick={() => {this.setState({page: 2})}}>Create</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink onClick={() => {this.setState({page: 3})}}>About</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-        <Container>
-          <Row>
-            <Col xs="3"></Col>
-            <Col>
-              <Page quests={this.getQuests()} page={this.state.page} />
-            </Col>
-            <Col xs="3"></Col>
-          </Row>
-        </Container>
-        <Footer/>
-      </div>
+      <Router>
+        <Fragment>
+          <div className="App"> 
+            <Nav />
+            <Route path="/" exact component={Create} />
+            <Route path="/quests/" exact component={Create} />
+            <Route path="/create" component={Create} />
+            <Route path="/faq/" component={Create} />
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+const Nav = (props) => {
+  //https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180
+  return (
+    <div className="nav">
+      
+    </div>
+  )
+}
+
+export default Nav;
